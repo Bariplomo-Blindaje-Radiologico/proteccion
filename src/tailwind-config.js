6e39b5b08d16c82bc1dccb635598b4d77f05b2ba
Bariplomo-Blindaje-Radiologico <bariplomo@gmail.com>
2026-08-25 23:44:02 -0600

    Add files via upload

---
+++ src/tailwind-config.js
@@ -0,0 +1,152 @@
+window.tailwind = window.tailwind || {};
+
+  tailwind.config = {
+    darkMode: "class",
+    theme: {
+      extend: {
+        "colors": {
+                "surface-tint": "#565f6e",
+                "on-secondary": "#ffffff",
+                "surface-container-high": "#e6e8ea",
+                "on-primary-fixed-variant": "#3e4755",
+                "inverse-primary": "#bec7d8",
+                "secondary-fixed": "#d3e4ff",
+                "inverse-on-surface": "#eff1f3",
+                "outline": "#75777c",
+                "on-tertiary": "#ffffff",
+                "background": "#f8f9fb",
+                "error": "#ba1a1a",
+                "surface-container-highest": "#e0e3e5",
+                "on-surface-variant": "#45474c",
+                "secondary": "#4f6076",
+                "inverse-surface": "#2d3133",
+                "tertiary": "#000000",
+                "on-tertiary-fixed-variant": "#5c4300",
+                "outline-variant": "#c5c6cc",
+                "on-background": "#191c1e",
+                "on-primary-container": "#7b8494",
+                "tertiary-container": "#261900",
+                "on-primary": "#ffffff",
+                "on-primary-fixed": "#131c28",
+                "surface-container-lowest": "#ffffff",
+                "tertiary-fixed": "#ffdea0",
+                "on-secondary-container": "#53647a",
+                "lead-gray": "#4A5568",
+                "secondary-fixed-dim": "#b7c8e2",
+                "surface": "#f8f9fb",
+                "on-error": "#ffffff",
+                "surface-bright": "#f8f9fb",
+                "primary-fixed": "#dae3f4",
+                "surface-container": "#eceef0",
+                "secondary-container": "#d0e1fc",
+                "error-container": "#ffdad6",
+                "on-tertiary-fixed": "#261900",
+                "primary-fixed-dim": "#bec7d8",
+                "surface-variant": "#e0e3e5",
+                "surface-container-low": "#f2f4f6",
+                "on-secondary-fixed": "#0a1c30",
+                "on-tertiary-container": "#a47e27",
+                "safety-yellow": "#DDB256",
+                "primary-container": "#131c28",
+                "emergency-blue": "#0047AB",
+                "pure-white": "#FFFFFF",
+                "primary": "#000000",
+                "on-secondary-fixed-variant": "#37485d",
+                "surface-dim": "#d8dadc",
+                "tertiary-fixed-dim": "#edc063",
+                "on-error-container": "#93000a",
+                "on-surface": "#191c1e"
+        },
+        "borderRadius": {
+                "DEFAULT": "0.125rem",
+                "lg": "0.25rem",
+                "xl": "0.5rem",
+                "full": "0.75rem"
+        },
+        "spacing": {
+                "gutter": "24px",
+                "max-width": "1280px",
+                "margin-mobile": "16px",
+                "margin-desktop": "64px",
+                "base": "8px"
+        },
+        "fontFamily": {
+                "headline-lg": [
+                        "Inter"
+                ],
+                "body-lg": [
+                        "Inter"
+                ],
+                "headline-lg-mobile": [
+                        "Inter"
+                ],
+                "title-md": [
+                        "Inter"
+                ],
+                "body-md": [
+                        "Inter"
+                ],
+                "display-lg": [
+                        "Inter"
+                ],
+                "label-sm": [
+                        "Inter"
+                ]
+        },
+        "fontSize": {
+                "headline-lg": [
+                        "32px",
+                        {
+                                "lineHeight": "40px",
+                                "letterSpacing": "-0.01em",
+                                "fontWeight": "600"
+                        }
+                ],
+                "body-lg": [
+                        "18px",
+                        {
+                                "lineHeight": "28px",
+                                "fontWeight": "400"
+                        }
+                ],
+                "headline-lg-mobile": [
+                        "28px",
+                        {
+                                "lineHeight": "36px",
+                                "fontWeight": "600"
+                        }
+                ],
+                "title-md": [
+                        "20px",
+                        {
+                                "lineHeight": "28px",
+                                "fontWeight": "600"
+                        }
+                ],
+                "body-md": [
+                        "16px",
+                        {
+                                "lineHeight": "24px",
+                                "fontWeight": "400"
+                        }
+                ],
+                "display-lg": [
+                        "48px",
+                        {
+                                "lineHeight": "56px",
+                                "letterSpacing": "-0.02em",
+                                "fontWeight": "700"
+                        }
+                ],
+                "label-sm": [
+                        "12px",
+                        {
+                                "lineHeight": "16px",
+                                "letterSpacing": "0.05em",
+                                "fontWeight": "600"
+                        }
+                ]
+        }
+},
+    },
+  }
